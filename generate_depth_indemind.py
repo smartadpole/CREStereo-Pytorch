cfbb827c534dd918ac700a694aa0fa4f92e41c4d
donglijian@indemind.cn <donglijian@indemind.cn>
2023-02-16 16:43:54 +0800

    change scale gray to 0,255 and 255-

--- generate_depth_indemind.py
+++ generate_depth_indemind.py
@@ -61,6 +61,15 @@ def GetImages(path, flag='kitti'):
 
     return left_files, right_files, root_len
 
+def scale_reduce(gray_img):
+    image = gray_img - np.min(gray_img)
+    if (np.max(image)) <= 0:
+        print("return gray img")
+        return gray_img
+    image = image / np.max(image) * 255.0
+    image = 255 - image
+    return image
+
 def gray_scale_region(gray_img, min_value=1, max_value=255):
     min_valid_index = gray_img > min_value
 
@@ -91,7 +100,7 @@ def get_file_name(file_name, image_path):
     depth_list = []
     for image_name in file_lists:
         image_full_path = os.path.join(image_path, image_name)
-        image_dest_path = image_full_path.replace("REMAP", "DEPTH/CREStereo").split()[0]
+        image_dest_path = image_full_path.replace("REMAP", "DEPTH/CREStereo_reduce").split()[0]
         image_dest_path = image_dest_path.replace(".jpg", ".png")
         image_list.append(image_full_path)
         depth_list.append(image_dest_path)
@@ -161,7 +170,8 @@ def write_by_img_list(img_list, depth_list, model):
 
         MkdirSimple(depth_image_file)
 
-        predict_np_scale = gray_scale_region(predict_np, 5, 240)
+       # predict_np_scale = gray_scale_region(predict_np, 5, 240)
+        predict_np_scale = scale_reduce(predict_np)
         if (not predict_np_scale.any()):
             print(left_image_file)
             continue
